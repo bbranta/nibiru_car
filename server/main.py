bf6fd60c9b98d19e05005bb35a4d6598f5d1bdcc
Bruno Branta Lopes <branta@motorola.com>
2015-12-14 21:21:32 -0200

    API Testing

--- server/main.py
+++ server/main.py
@@ -1,7 +1,8 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, render_template, jsonify, abort, make_response
+
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -13,10 +14,26 @@ def hello():
     return 'Hello World!'
 
 
+@app.route('/list/')
+def list_entries():
+    return render_template('show_entries.html', entries=[])
+
+
+@app.route('/api/v1/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': []})
+
+
+@app.route('/api/v1/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    if task_id <= 0:
+        abort(404)
+    return jsonify({'task': task_id})
+
+
 @app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, Nothing at this URL.', 404
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.errorhandler(500)
