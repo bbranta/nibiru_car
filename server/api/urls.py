36d8a02814b07966324c778b4b1dd4100cd55d5e
Bruno Branta Lopes <branta@motorola.com>
2015-12-14 23:59:39 -0200

    Creating sample model.

--- server/api/urls.py
+++ server/api/urls.py
@@ -6,4 +6,5 @@ def init(app):
                      methods=['GET'])
     app.add_url_rule('/api/v1/tasks/<int:task_id>', view_func=views.get_task,
                      methods=['GET'])
-
+    app.add_url_rule('/api/v1/tasks/<int:task_id>/add',
+                     view_func=views.add_task, methods=['GET'])
--- server/api/views.py
+++ server/api/views.py
@@ -1,11 +1,21 @@
+from api.models.greeting import Greeting
 from flask import jsonify, abort
 
 
 def get_tasks():
-    return jsonify({'tasks': []})
+    return jsonify({
+        'tasks': [str(s) for s in Greeting.query_all()],
+    })
 
 
 def get_task(task_id):
     if task_id <= 0:
         abort(404)
     return jsonify({'task': task_id})
+
+
+def add_task(task_id):
+    greeting = Greeting.get_or_insert(str(task_id))
+    greeting.content = "Bl2a " + str(task_id)
+    greeting.put()
+    return jsonify({'task': str(greeting)})
