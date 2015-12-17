2935cb10538c6689078831c5faba39c08b844345
Bruno Branta Lopes <branta@motorola.com>
2015-12-17 15:24:59 -0200

    Implements adding car

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,9 +2,7 @@ import views
 
 
 def init(app):
-    app.add_url_rule('/api/v1/cars', view_func=views.list_cars,
+    app.add_url_rule('/api/v1/cars', view_func=views.cars,
+                     methods=['GET', 'POST'])
+    app.add_url_rule('/api/v1/add_dummy_car', view_func=views.add_dummy_car,
                      methods=['GET'])
-    app.add_url_rule('/api/v1/tasks/<int:task_id>', view_func=views.get_task,
-                     methods=['GET'])
-    app.add_url_rule('/api/v1/tasks/<int:task_id>/add',
-                     view_func=views.add_task, methods=['GET'])
--- server/api/views.py
+++ server/api/views.py
@@ -1,46 +1,79 @@
 # coding=utf-8
+import logging
+import random
+
 from api.models.car import Car
-from api.models.greeting import Greeting
 from api.models.user import User
-from flask import jsonify, abort
+from flask import jsonify, request
+
+logger = logging.getLogger(__name__)
 
 
-def get_tasks():
-    return jsonify({
-        'tasks': [str(s) for s in Greeting.query_all()],
-    })
-
-
-def get_task(task_id):
-    if task_id <= 0:
-        abort(404)
-    return jsonify({'task': task_id})
-
-
-def add_task(task_id):
-    greeting = Greeting.get_or_insert(str(task_id))
-    greeting.content = 'Bl2a ' + str(task_id)
-    greeting.put()
-    return jsonify({'task': str(greeting)})
-
-def dummy():
+def add_dummy_car():
     u = User.get_user('351.394.078.80')
     u.name = 'Mário do'
     u.phone = '(11) 223-244-523'
     u.driver_license = '123.55.13.2.13.23'
     u_key = u.put()
 
-    c = Car.get_car('ABC-1236')
+    license_plate = ''.join(random.choice('ABCDEFGH') for _ in range(3)) + \
+                    ''.join(random.choice('1234567890') for _ in range(4))
+    c = Car.get_car(license_plate)
     c.brand = 'Mercedes'
-    c.model = 'Classe B'
+    c.model = 'Classe ' + random.choice('ABCD')
     c.year = 2007
     c.optionals = ['Ar Condicionado', 'Trio Elétrica']
     c.price = 250
     c.fuel = 'Gasolina'
     c.available_slots = ['8:00', '10:00', '11:00']
     c.owner = u_key
-    c.put()
-    return jsonify({})
+    c_key = c.put()
+    return jsonify({
+        'user': u_key.id(),
+        'car': c_key.id(),
+    })
+
+
+def cars():
+    if request.method == 'POST':
+        return add_car()
+    else:
+        return list_cars()
+
+
+def add_car():
+    try:
+        logging.info("NOVOO CARROOOO %r", request.form)
+        logging.info("NOVOO OWNER %r", request.form['owner_cpf'])
+        u = User.get_user(request.form['owner_cpf'])
+        u.name = request.form.get('owner_name')
+        u.phone = request.form.get('owner_phone')
+        u.driver_license = request.form.get('owner_driver_license')
+        u_key = u.put()
+
+        c = Car.get_car(request.form.get('license_plate'))
+        c.brand = request.form.get('brand')
+        c.model = request.form.get('model')
+        c.year = int(request.form.get('year'))
+        c.optionals = request.form.get('optionals')
+        c.price = int(request.form.get('price'))
+        c.fuel = request.form.get('fuel')
+        c.available_time_range = request.form.get('available_time_range')
+        c.owner = u_key
+        c_key = c.put()
+
+        return jsonify({
+            'status': 'ok',
+            'message': 'Carro criado com sucesso!',
+            'car_key': c_key.id(),
+        })
+
+    except Exception, e:
+        logger.error("Error saving car %r", e)
+        return jsonify({
+            'status': 'fail',
+            'message': e.message,
+        })
 
 
 def list_cars():
